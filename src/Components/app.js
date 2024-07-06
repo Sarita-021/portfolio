5786dd47d43d3b9ea19e14834af69ce4efdedabd
Sarita <121181405+Sarita-021@users.noreply.github.com>
2024-07-06 23:14:38 +0530

    moved app.css

--- src/Components/app.js
+++ src/Components/app.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
-import "../app.css"
+import "../CSS/app.css";
 import Header from "./header";
 import Home from "../Pages/home";
 import Footer from "./footer";
